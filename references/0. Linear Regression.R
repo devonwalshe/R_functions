2a333568c6c84bef10f8a98cda1f2d6eb21bc136
Azymuth <devon.walshe@gmail.com>
2017-05-22 21:52:19 +0100

    finished with neural network model test

--- references/0. Linear Regression.R
+++ references/0. Linear Regression.R
@@ -14,7 +14,55 @@ cplot = ggcorrplot(corr, method='circle', hc.order=TRUE , outline.col='white', l
 set.seed(90210)
 smp_size = floor(0.30 * nrow(iris))
 sample_ind = sample(seq_len(nrow(iris)), size=smp_size)
-sample = iris[sample_ind,]
-test = iris[-sample_ind,]
+iris_sample = iris[sample_ind,]
+iris_test = iris[-sample_ind,]
 
+### fit a model between width and length
+iris_fit = lm(Petal.Length ~ Petal.Width, data=sample)
+
+### predict petal length on the test group
+length_pred = predict(iris_fit, newdata= iris_test)
+iris_test$length_pred = length_pred
+
+### Calculate error of the model
+iris_test = iris_test %>% mutate(prediction_error = length_pred-Petal.Length)
+
+### Now try a multivariate regression
+library(ggcorrplot)
+car_corr = round(cor(mtcars), 3)
+car_corr_p = cor_pmat(mtcars) # significance values for each one
+car_cplot = ggcorrplot(car_corr, method='square', hc.order=TRUE , type=c('lower'), 
+                       colors=c('red', 'white', 'green'), outline.col='white', 
+                       lab='true', p.mat=car_corr_p, insig='blank', show.legend=FALSE, lab_size=3)
+
+### Apply factors to vs, am, cyl, gear and carb
+mtcars_factor = mtcars
+mtcars_factor[,c("vs", "am", "cyl", "gear", "carb")]  = lapply(mtcars_factor[,c("vs", "am", "cyl", "gear", "carb")], as.factor)
+
+### Fit model for mpg
+car_fit = lm(mpg ~ cyl + disp + hp + drat + wt + qsec + vs + am+gear+carb, data=mtcars )
+car_factor_fit = lm(mpg ~ cyl + disp + hp + drat + wt + qsec + vs + am+gear+carb, data=mtcars_factor )
+
+### Test it out
+mtcars$mpg_vanilla_pred = predict(car_fit, mtcars)
+mtcars$mpg_factor_pred = predict(car_factor_fit, mtcars_factor)
+
+### Plot the error
+library(reshape2)
+mtcars$model = row.names(mtcars)
+mtmelt = melt(mtcars[,c('model', 'mpg', 'mpg_factor_pred', 'mpg_vanilla_pred')], id.vars="model")
+
+mtmelt$model = factor(mtmelt$model, levels = row.names(mtcars[order(mtcars$mpg),]))
+ggplot(mtmelt, aes(x=model, y=value, group=variable, colour=variable)) +
+  geom_line(aes(group=variable)) +
+  theme(axis.text.x = element_text(angle=90, vjust=.5, hjust=1),
+        panel.background = element_blank(), axis.line=element_line(size=0.2),
+        panel.grid.major=element_line(colour="#dddddd", size=0.1))
+
+# interesting the factor model has less overall error
+mtcars = mtcars %>% mutate(v_error = mpg_vanilla_pred-mpg, f_error = mpg_factor_pred-mpg) 
+sum(mtcars$v_error^2)
+sum(mtcars$f_error^2)
+
+# Thats enough for now
 
--- references/1. Neural Networks.R
+++ references/1. Neural Networks.R
@@ -1,4 +1,5 @@
 ### Neural networks
+library(dplyr, nnet)
 
 ### Data
 
@@ -9,4 +10,43 @@ train_ind = sample(seq_len(nrow(iris)), size=smp_size)
 
 ### Split into more even sample with caret
 library(caret)
-createDataPartition(iris, times = 1, p = 0.7, list = TRUE, groups = min(1, length(iris)))
+train_ind = createDataPartition(iris$Species, times = 4, p = 0.7, list = TRUE, groups = min(1, length(iris)))[[1]]
+i_train = iris[train_ind,]
+i_test = iris[-train_ind,]
+
+### Try single layer neuralnet with nnet
+library(nnet)
+i_fit = nnet(Species~Sepal.Length+Sepal.Width+Petal.Length+Petal.Width, 
+             size=30, 
+             data = iris, 
+             maxit=1000)
+
+i_cm = confusionMatrix(i_test$Species, i_test$prediction)
+
+i_test$prediction = colnames(predict(i_fit, i_test)[,apply(predict(i_fit, i_test), 1, function(x) which(x==max(x)))])
+
+### Try out neuralnet
+library(neuralnet)
+
+### Convert train to model matrix with categorical output vars as binary
+train_m = data.frame(model.matrix(~ . +Species+0, data=i_train))
+
+### Fit
+n = colnames(train_m)
+nn_form = as.formula(paste(paste(n[grep("Species", n)], collapse = " + "), " ~ " , paste(n[-grep("Species", n)], collapse = " + "), sep=""))
+nn_ifit = neuralnet(nn_form, 
+                    train_m,
+                    hidden = c(3,10),
+                    lifesign="full",
+                    stepmax="1e+05",
+                    threshold = 0.01,
+                    rep=3)
+
+### Inspect the fit
+summary(nn_ifit)
+nn_prediction = compute(nn_ifit, i_test[-5])
+pred_id = apply(nn_prediction$net.result, 1, which.max)
+prediction = c("setosa", "versicolor", "virginica")[pred_id]
+confusionMatrix(i_test$Species, prediction)
+
+
